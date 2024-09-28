a3fe0e3e76d032ef4d4d78909028373734900ae8
Meliq Pilosyan <melopilosyan@hey.com>
2024-09-28 22:19:02 +0400

    Introduces the progress animation feature

--- lua/rspec/utils/notif.lua
+++ lua/rspec/utils/notif.lua
@@ -1,12 +1,18 @@
-local LL = vim.log.levels
-local MAX_NOTIFY_TIMEOUT = 60 * 60 * 1000
+local nvim_notify_loaded, _ = pcall(require, "notify")
 
-local function notify(msg, log_level, title, predecessor)
-  return vim.notify(msg, log_level, {
-    title   = title,
-    timeout = predecessor and 3000 or MAX_NOTIFY_TIMEOUT,
-    replace = predecessor,
-  })
+local LL = vim.log.levels
+local Icons = {
+  ERROR = " ",
+  INFO = " ",
+}
+
+local icon_frames = { "⣷", "⣯", "⣟", "⡿", "⢿", "⣻", "⣽", "⣾" }
+icon_frames.count = #icon_frames
+
+---@param notif rspec.Notif
+function icon_frames:next_for(notif)
+  notif.frame = (notif.frame + 1) % self.count
+  return self[notif.frame]
 end
 
 ---@param spec rspec.Spec
@@ -18,27 +24,66 @@ local function openning_notification_title(spec)
 end
 
 ---@class rspec.Notif
----@field predecessor any nvim-notify plugin record
+---@field package frame number
+---@field private predecessor any nvim-notify record
 local Notif = {}
 
 ---@param spec rspec.Spec
 function Notif:new(spec)
   self.__index = self
 
-  return setmetatable({
-    predecessor = notify(table.concat(spec.cmd, " "), LL.WARN, openning_notification_title(spec))
+  local instance = setmetatable({
+    frame = 1,
+    predecessor = vim.notify(table.concat(spec.cmd, " "), LL.WARN, {
+      title = openning_notification_title(spec),
+      icon = icon_frames[1],
+      timeout = false,
+    })
   }, self)
+
+  if nvim_notify_loaded then instance:show_loading() end
+
+  return instance
 end
 
 ---@param msg string
 function Notif:success(msg)
-  notify(msg, LL.INFO, "RSpec: Succeeded", self.predecessor)
+  self:complete(msg, "INFO", "RSpec: Succeeded")
 end
 
 ---@param msg string
 ---@param title? string
 function Notif:failure(msg, title)
-  notify(msg, LL.ERROR, title or "RSpec: Failed", self.predecessor)
+  self:complete(msg, "ERROR", title or "RSpec: Failed")
+end
+
+---@private
+---@param msg string
+---@param level string
+---@param title string
+function Notif:complete(msg, level, title)
+  self.frame = nil
+
+  self.predecessor = vim.notify(msg, LL[level], {
+    icon = Icons[level],
+    title = title,
+    timeout = 3000,
+    replace = self.predecessor,
+    hide_from_history = false,
+  })
+end
+
+---@private
+function Notif:show_loading()
+  if not self.frame then return end
+
+  self.predecessor = vim.notify(nil, nil, {
+    icon = icon_frames:next_for(self),
+    replace = self.predecessor,
+    hide_from_history = true,
+  })
+
+  vim.defer_fn(function() self:show_loading() end, 150)
 end
 
 return Notif
